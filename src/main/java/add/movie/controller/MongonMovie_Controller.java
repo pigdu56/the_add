f46b06c58e9de06e445dcb34c052de4a95efe641
junpyo <junpyo@10.0.0.73>
2019-04-04 12:31:39 +0900

    movie

--- src/main/java/add/movie/controller/MongonMovie_Controller.java
+++ src/main/java/add/movie/controller/MongonMovie_Controller.java
@@ -1,5 +1,17 @@
 package add.movie.controller;
 
-public class MongonMovie_Controller {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@org.springframework.stereotype.Controller
+@RequestMapping("/movie")
+
+public class MongonMovie_Controller {
+	
+	@RequestMapping("Preference")
+	public ModelAndView Preference() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/preference");
+		return mv;
+	}
 }
