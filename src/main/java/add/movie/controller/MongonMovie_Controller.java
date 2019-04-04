a011263512a8862ab8d2c1a5a5cb8ee693601d6e
junpyo <junpyo@10.0.0.73>
2019-04-04 14:44:50 +0900

    movie

--- src/main/java/add/movie/controller/MongonMovie_Controller.java
+++ src/main/java/add/movie/controller/MongonMovie_Controller.java
@@ -1,17 +1,27 @@
 package add.movie.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import add.movie.Mongo;
 
 @org.springframework.stereotype.Controller
 @RequestMapping("/movie")
-
 public class MongonMovie_Controller {
 	
-	@RequestMapping("Preference")
+	@Autowired
+	Mongo mg;
+	
+	@RequestMapping(value = {"/preference"}, method = RequestMethod.GET)
 	public ModelAndView Preference() {
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("/preference");
+		
+		mv.addObject("rating", mg.getRating());
+		mv.addObject("cinema", mg.getCinema());
+		mv.addObject("genre", mg.getGenre());
+		mv.setViewName("preference");
 		return mv;
 	}
 }
--- src/main/webapp/WEB-INF/views/pages/movie/preference.jsp
+++ src/main/webapp/WEB-INF/views/pages/movie/preference.jsp
@@ -1,5 +1,23 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <div class="container">
-
+	<h1>장르</h1>
+	<ul>
+		<c:forEach var="i" items="${genre}">
+			<li>${i._id} : ${i.count}</li>
+		</c:forEach>
+	</ul>
+	<h1>연령</h1>
+	<ul>
+		<c:forEach var="i" items="${rating}">
+			<li>${i._id} : ${i.count}</li>
+		</c:forEach>
+	</ul>
+	<h1>영화관</h1>
+	<ul>
+		<c:forEach var="i" items="${cinema}">
+			<li>${i._id} : ${i.count}</li>
+		</c:forEach>
+	</ul>
 </div>
