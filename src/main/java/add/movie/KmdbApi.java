0c246bcb12ce72782d30c59ce8c69f1220dfcc6b
leejunpyo <ddd3380@naver.com>
2019-04-08 14:14:56 +0900

    movie

--- src/main/java/add/movie/KmdbApi.java
+++ src/main/java/add/movie/KmdbApi.java
@@ -20,16 +20,12 @@ public class KmdbApi {
 	public HashMap<String, String> kmdb(String movieNm, String openDt) throws Exception{
 		/*URL*/
 		StringBuilder urlBuilder = new StringBuilder("http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json.jsp?collection=kmdb_new"); 
-		
 		/*Service Key*/ 
 		urlBuilder.append("&" + URLEncoder.encode("ServiceKey","UTF-8")+"="+URLEncoder.encode("5CXKMULO4I6N586J47J3", "UTF-8")); 
-		
 		/*상세정보*/
 		urlBuilder.append("&" + URLEncoder.encode("detail","UTF-8") + "=" + URLEncoder.encode("Y", "UTF-8")); 
-	
 		/*영화제목*/
 		urlBuilder.append("&" + URLEncoder.encode("title","UTF-8") + "=" + URLEncoder.encode(movieNm, "UTF-8")); 
-		
 		/*개봉일자*/		
 		openDt	= openDt.replaceAll("[-]", "");		
 		urlBuilder.append("&" + URLEncoder.encode("releaseDts","UTF-8") + "=" + URLEncoder.encode(openDt, "UTF-8")); 
